df6440af806f3f1290f0b19d187d3ee6d1f85233
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-05-05 13:25:24 +0530

    style : JobList css added

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,5 +1,13 @@
-
-import  { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+import {
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardContent,
+  Link,
+  CircularProgress,
+} from "@material-ui/core";
 
 const JobList = () => {
   const [data, setData] = useState([]);
@@ -29,14 +37,14 @@ const JobList = () => {
       );
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const result = await response.json();
-      setData(prevData => [...prevData, ...result.jdList]);
-      setCurrentPage(prevPage => prevPage + 1);
+      setData((prevData) => [...prevData, ...result.jdList]);
+      setCurrentPage((prevPage) => prevPage + 1);
     } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
+      console.error("There was a problem with the fetch operation:", error);
     } finally {
       setLoading(false);
     }
@@ -74,25 +82,171 @@ const JobList = () => {
   }, [loading, currentPage]);
 
   return (
-    <div>
-      <h1>Job Cards</h1>
-      <div className="job-card-container">
+    <Container>
+      <Typography variant="h1" gutterBottom>
+        Weekday
+      </Typography>
+      <Grid container spacing={8}>
         {data.map((job, index) => (
-          <div key={index} className="job-card">
-            <h2>{job.jobRole}</h2>
-            <h3>{job.companyName}</h3>
-            <p>Location: {job.location}</p>
-            <p>Description: {job.jobDetailsFromCompany}</p>
-            <p>Experience: {job.minExp} - {job.maxExp} years</p>
-            <a href={job.jdLink}>Apply Now</a>
-          </div>
+          <Grid item key={index} xs={12} sm={6} md={4}>
+            <Card className="job-card" style={{ marginBottom: "10px" }}>
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  component="h2"
+                  style={{ color: "rgb(139, 139, 139)" }}
+                >
+                  {job.companyName}
+                </Typography>
+                <Typography
+                  variant="h6"
+                  color="textSecondary"
+                  gutterBottom
+                  style={{ color: "gba(0, 0, 0, 0.87)" }}
+                >
+                  {job.jobRole.charAt(0).toUpperCase() + job.jobRole.slice(1)}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="p"
+                  style={{
+                    fontWeight: 500,
+                    color: "#000000de",
+                    fontSize: "16px",
+                    lineHeight: "20px",
+                  }}
+                >
+                  {job.location.charAt(0).toUpperCase() + job.location.slice(1)}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="p"
+                  style={{
+                    fontWeight: 500,
+                    color: "rgb(77, 89, 106)",
+                    fontSize: "16px",
+                    lineHeight: "20px",
+                    marginTop: "1rem",
+                  }}
+                >
+                  Estimated Salary: {job.minJdSalary}k - {job.maxJdSalary}k USD
+                </Typography>
+
+                <Typography
+                  variant="body2"
+                  component="h3"
+                  style={{
+                    fontWeight: 500,
+                    fontSize: "16px",
+                    lineHeight: "20px",
+                    marginTop: "1rem",
+                  }}
+                >
+                  Description:
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="h3"
+                  style={{
+                    maxHeight: "160px",
+                    overflow: "hidden",
+                    position: "relative",
+                    marginTop: "5px",
+                  }}
+                >
+                  {job.jobDetailsFromCompany}
+                  <div
+                    style={{
+                      position: "absolute",
+                      bottom: 0,
+                      width: "100%",
+                      textAlign: "center",
+                      background:
+                        "linear-gradient(to top, rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 1))",
+                    }}
+                  >
+                    <Link
+                      variant="body2"
+                      style={{
+                        textDecoration: "none",
+                        color: "#007bff",
+                        cursor: "pointer",
+                        fontWeight:"500",
+                        backgroundColor:"#fff"
+                      }}
+                    >
+                      View More
+                    </Link>
+                  </div>
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="p"
+                  style={{
+                    fontWeight: 600,
+                    color: "rgb(139, 139, 139)",
+                    fontSize: "16px",
+                    lineHeight: "20px",
+                    marginTop: "1rem",
+                  }}
+                >
+                  Min Experience: 
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="p"
+                  style={{
+                    fontWeight: 400,
+                    color: "rgba(0, 0, 0, 0.87)",
+                    fontSize: "16px",
+                    lineHeight: "20px",
+                    marginTop: "0.5rem",
+                  }}
+                >
+                  {job.minExp === null ? 0 : job.minExp} years
+                </Typography>
+                <div
+                  style={{
+                    width: "100%",
+                    backgroundColor: "#55EFC4",
+                    margin: "1rem auto",
+                    textAlign: "center",
+                    borderRadius: "5px",
+                  }}
+                >
+                  <Link
+                    href={job.jdLink}
+                    variant="body2"
+                    style={{
+                      color: "rgb(0, 0, 0)",
+                      textDecoration: "none",
+                      display: "block",
+                      padding: "10px",
+                    }}
+                  >
+                    ⚡ Easy Apply
+                  </Link>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
         ))}
-        {loading && <p>Loading...</p>}
-        <div id="observer" style={{ height: "10px" }}></div>
-      </div>
-    </div>
+      </Grid>
+      {loading && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      )}
+      <div id="observer" style={{ height: "10px" }}></div>
+    </Container>
   );
 };
 
 export default JobList;
-
